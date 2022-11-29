076cd97a337e3ffaf4dfc6183e3e8efd5cee7e3d
Alexander van der Werff <avdwerff@gmail.com>
2022-11-29 22:48:15 +0100

    remove

--- Sources/BasedCplusplusClient/universal-based.build/Release-iphoneos/based.build/Objects-normal/arm64/utility.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/francesco/saulx/based/packages/cpp-client/src/utility.cpp \
-  /Users/francesco/saulx/based/packages/cpp-client/src/utility.hpp \
-  /Users/francesco/saulx/based/packages/cpp-client/lib/zlib/zlib.h \
-  /Users/francesco/saulx/based/packages/cpp-client/lib/zlib/zconf.h \
-  /Users/francesco/saulx/based/packages/cpp-client/lib/nlohmann-json/json.hpp
--- Sources/BasedCplusplusClient/universal-based.build/Release-iphoneos/linked-example.build/Objects-normal/arm64/example.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/francesco/saulx/based/packages/cpp-client/example/example.cpp \
-  /Users/francesco/saulx/based/packages/cpp-client/src/based.hpp
