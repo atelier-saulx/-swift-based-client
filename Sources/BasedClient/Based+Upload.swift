3fcc31ab1f39e1a5581cf86c63022a84ac1ccfd0
Alexander van der Werff <avdwerff@gmail.com>
2023-10-22 20:04:23 +0200

    fix merge conflict

--- Sources/BasedClient/Based+Upload.swift
+++ Sources/BasedClient/Based+Upload.swift
@@ -38,32 +38,32 @@ public enum UploadStatus {
 final class Uploader: NSObject {
     typealias Percentage = Double
     typealias Publisher = AnyPublisher<UploadStatus, Error>
-
+    
     private typealias Subject = CurrentValueSubject<UploadStatus, Error>
-
+    
     private lazy var urlSession = URLSession(
         configuration: .default,
         delegate: self,
         delegateQueue: .main
     )
-
+    
     private var subjectsByTaskID = [Int: Subject]()
-
+    
     func uploadFile(_ upload: Upload) -> Publisher {
         let subject = Subject(.progress(0))
         var removeSubject: (() -> Void)?
-
+        
         var request = URLRequest(
             url: upload.targetUrl,
             cachePolicy: .reloadIgnoringLocalCacheData
         )
-
+        
         request.httpMethod = "POST"
         request.setValue("blob", forHTTPHeaderField: "Req-Type")
         request.setValue("chunked", forHTTPHeaderField: "Transfer-Encoding")
         request.setValue(upload.token, forHTTPHeaderField: "JSON-authorization")
         request.setValue(upload.mimeType ?? "text/plain", forHTTPHeaderField: "Content-Type")
-
+        
         let task: URLSessionUploadTask
         switch upload.uploadType {
         case .file(let fileURL):
@@ -75,10 +75,9 @@ final class Uploader: NSObject {
                         subject.send(completion: .failure(error))
                         return
                     }
-//                    if let data = data, let res = try? JSONDecoder().decode([String: String].self, from: data) {
-//                        subject.send(.uploaded(id: res["id"]))
-//                    }
-                    subject.send(.uploaded(id: upload.id))
+                    if let data = data, let res = try? JSONDecoder().decode([String: String].self, from: data) {
+                        subject.send(.uploaded(id: res["id"]))
+                    }
                     subject.send(completion: .finished)
                     removeSubject?()
                 }
@@ -92,23 +91,22 @@ final class Uploader: NSObject {
                         subject.send(completion: .failure(error))
                         return
                     }
-//                    if let data = data, let res = try? JSONDecoder().decode([String: String].self, from: data) {
-//                        subject.send(.uploaded(id: res["id"]))
-//                    }
-                    subject.send(.uploaded(id: upload.id))
+                    if let data = data, let res = try? JSONDecoder().decode([String: String].self, from: data) {
+                        subject.send(.uploaded(id: res["id"]))
+                    }
                     subject.send(completion: .finished)
                     removeSubject?()
                 }
             )
         }
-
+        
         subjectsByTaskID[task.taskIdentifier] = subject
         removeSubject = { [weak self] in
             self?.subjectsByTaskID.removeValue(forKey: task.taskIdentifier)
         }
-
+        
         task.resume()
-
+        
         return subject.eraseToAnyPublisher()
     }
 }
@@ -128,14 +126,14 @@ extension Uploader: URLSessionTaskDelegate {
 }
 
 extension Based {
-
+    
     /**
      upload function is used to upload a file to a specified URL.
      - Parameters:
-        - fileUrl: A `URL` object representing the file to be uploaded.
-        - targetUrl: (Optional) A `URL` object representing the target URL where the file should be uploaded. If not provided, the file will be uploaded to the default target URL.
-        - mimeType: (Optional) A `String` representing the mime type of the file. If not provided, the mime type will be inferred from the file extension.
-        - name: (Optional) A `String` representing the name of the file. If not provided, the file name will be used.
+     - fileUrl: A `URL` object representing the file to be uploaded.
+     - targetUrl: (Optional) A `URL` object representing the target URL where the file should be uploaded. If not provided, the file will be uploaded to the default target URL.
+     - mimeType: (Optional) A `String` representing the mime type of the file. If not provided, the mime type will be inferred from the file extension.
+     - name: (Optional) A `String` representing the name of the file. If not provided, the file name will be used.
      - Returns: An `AnyPublisher<UploadStatus, Error>` object that emits an `UploadStatus` value upon successful completion and an `Error` object in case of failure.
      */
     public func upload(
@@ -147,14 +145,14 @@ extension Based {
     ) -> AnyPublisher<UploadStatus, Error> {
         return _upload(options: UploadOptions(uploadType: .file(fileUrl), targetUrl: targetUrl, name: name, mimeType: mimeType, authToken: authToken))
     }
-
+    
     /**
      upload function is used to upload a file to a specified URL.
      - Parameters:
-        - data: Data object representing the file to be uploaded.
-        - targetUrl: (Optional) A `URL` object representing the target URL where the file should be uploaded. If not provided, the file will be uploaded to the default target URL.
-        - mimeType: (Optional) A `String` representing the mime type of the file. If not provided, the mime type will be inferred from the file extension.
-        - name: (Optional) A `String` representing the name of the file. If not provided, the file name will be used.
+     - data: Data object representing the file to be uploaded.
+     - targetUrl: (Optional) A `URL` object representing the target URL where the file should be uploaded. If not provided, the file will be uploaded to the default target URL.
+     - mimeType: (Optional) A `String` representing the mime type of the file. If not provided, the mime type will be inferred from the file extension.
+     - name: (Optional) A `String` representing the name of the file. If not provided, the file name will be used.
      - Returns: An `AnyPublisher<UploadStatus, Error>` object that emits an `UploadStatus` value upon successful completion and an `Error` object in case of failure.
      */
     public func upload(
@@ -166,7 +164,7 @@ extension Based {
     ) -> AnyPublisher<UploadStatus, Error> {
         return _upload(options: UploadOptions(uploadType: .data(data), targetUrl: targetUrl, name: name, mimeType: mimeType, authToken: authToken))
     }
-
+    
     private func _upload(options: UploadOptions) -> AnyPublisher<UploadStatus, Error> {
         Just(options)
             .setFailureType(to: Error.self)
@@ -174,19 +172,19 @@ extension Based {
                 guard
                     let self = self,
                     let targetUrl = URL(string: Current.basedClient.service(
-                            org: self.configuration.org,
-                            project: self.configuration.project,
-                            env: self.configuration.env,
-                            html: true
-                        )
+                        org: self.configuration.org,
+                        project: self.configuration.project,
+                        env: self.configuration.env,
+                        html: true
+                    )
                         .appending("/db:file-upload")
                     )
                 else {
                     throw BasedError.uploadError(message: "Could not create upload url")
                 }
-
+                
                 let urlEncodedJson = "{\"token\":\"\(options.authToken)\"}".addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) ?? ""
-
+                
                 return Upload(
                     uploadType: options.uploadType,
                     targetUrl: targetUrl,
@@ -194,7 +192,7 @@ extension Based {
                     mimeType: options.mimeType,
                     token: urlEncodedJson
                 )
-
+                
             }
             .flatMap { upload -> AnyPublisher<UploadStatus, Error> in
                 let uploader = Uploader()
@@ -202,7 +200,7 @@ extension Based {
             }
             .eraseToAnyPublisher()
     }
-
+    
 }
 
 extension Publisher {
